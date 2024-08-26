1ef754c47dd735f23eb4130e088fd5af7d26c925
Robb Kidd <robbkidd@honeycomb.io>
2024-08-26 16:43:53 -0400

    instrumented

--- services/backend-for-frontend-ruby/src/app.rb
+++ services/backend-for-frontend-ruby/src/app.rb
@@ -1,10 +1,13 @@
 require 'sinatra/base'
 require 'net/http'
+require_relative 'observability'
 
 class BackendForFrontendApp < Sinatra::Application
   set :bind, '0.0.0.0'
   set :port, '10115'
   set :show_exceptions, false
+
+  TRACER = OpenTelemetry.tracer_provider.tracer('BackendForFrontendApp', '0.1.0')
 
   PHRASE_URI = URI('http://phrase-picker:10118/phrase')
   IMAGE_URI = URI('http://image-picker:10116/imageUrl')
@@ -13,14 +16,18 @@ class BackendForFrontendApp < Sinatra::Application
   post '/createPicture' do
     meminated_response = nil
 
-    # get a phrase
-    phrase_response = Net::HTTP.get_response(PHRASE_URI)
-    phrase = JSON.parse(phrase_response.body).fetch('phrase') { return [500, 'Sorry. No phrases were found to meminate.'] }
-    # get an image
-    image_response = Net::HTTP.get_response(IMAGE_URI)
-    image_url = JSON.parse(image_response.body).fetch('imageUrl') { return [500, 'Sorry. No images were found to meminate.'] }
-    # meminate phrase onto image
-    meminated_response = Net::HTTP.post(MEMINATOR_URI, { phrase: phrase, imageUrl: image_url }.to_json, 'Content-Type' => 'application/json')
+    TRACER.in_span('create_picture') do |span|
+      # get a phrase
+      phrase_response = Net::HTTP.get_response(PHRASE_URI)
+      phrase = JSON.parse(phrase_response.body).fetch('phrase') { return [500, 'Sorry. No phrases were found to meminate.'] }
+      span.set_attribute('app.phrase', phrase)
+      # get an image
+      image_response = Net::HTTP.get_response(IMAGE_URI)
+      image_url = JSON.parse(image_response.body).fetch('imageUrl') { return [500, 'Sorry. No images were found to meminate.'] }
+      span.set_attribute('app.imageUrl', image_url)
+      # meminate phrase onto image
+      meminated_response = Net::HTTP.post(MEMINATOR_URI, { phrase: phrase, imageUrl: image_url }.to_json, 'Content-Type' => 'application/json')
+    end
 
     if meminated_response.nil?
       return [500, 'Sorry. Could not create a picture for you.']
---
+++ services/backend-for-frontend-ruby/src/observability.rb
@@ -0,0 +1,7 @@
+require 'opentelemetry/sdk'
+require 'opentelemetry/instrumentation/all'
+require 'opentelemetry-exporter-otlp'
+
+OpenTelemetry::SDK.configure do |c|
+  c.use_all()
+end
